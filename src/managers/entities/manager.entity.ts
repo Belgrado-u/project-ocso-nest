07f9d50cddac66889fc9a93bb72588bd68633db1
Belgrado-u <cheo2931@gmail.com>
2024-09-29 01:29:47 -0600

    fix:managerSalary type changed from string to number

--- src/managers/entities/manager.entity.ts
+++ src/managers/entities/manager.entity.ts
@@ -1,1 +1,16 @@
-export class Manager {}
+import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+
+@Entity()
+export class Manager {
+    @PrimaryGeneratedColumn('uuid')
+    managerId:string;
+    @Column('text')
+    managerFullName:string;
+    @Column('float')
+    managerSalary:number;
+    @Column('text')
+    managerEmail:string;
+    @Column('text')
+    managerPhoneNumber:string;
+    //Relacion con Location
+}
